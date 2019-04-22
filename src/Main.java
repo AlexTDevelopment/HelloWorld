d4ef70b4382f3385ca4cd628f2b46adc23bc4ab9
AlexTDevelopment <tikh@mail.ru>
2019-04-22 20:58:50 +0300

    test2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        boolean b = true;
+        boolean b = false;
+        boolean a = true;
+        boolean c = true;
+        a &= c;
+        a &= b;
+        a &= c;
+        System.out.println(a);
     }
 }
