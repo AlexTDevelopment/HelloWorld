968cbdacf1645c2263b6b78ef1103a5d8a082681
AlexTDevelopment <tikh@mail.ru>
2019-05-13 13:59:24 +0300

    test3

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,9 @@ public class Main {
         a &= b;
         a &= c;
         System.out.println(a);
+        Test t = new Test();
+        Test.Inner i = new Test.Inner();
+        System.out.println(i.j);
+        // to test commit in other branches
     }
 }
